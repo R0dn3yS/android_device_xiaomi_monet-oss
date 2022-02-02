f84d0fe997cb5555eff7fbedad47e3cd6a80a070
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-02-02 10:43:53 +0100

    Weeb Project bringup

--- weeb_monet.mk
+++ weeb_monet.mk
@@ -8,25 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common Weeb Project stuff.
+$(call inherit-product, vendor/weeb/config/common_full_phone.mk)
 
 # Inherit from monet device
 $(call inherit-product, device/xiaomi/monet/device.mk)
 
-PRODUCT_NAME := evolution_monet
+PRODUCT_NAME := weeb_monet
 PRODUCT_DEVICE := monet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 10 Lite 5G
 
-EVO_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
-
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_USES_AOSP_RECOVERY := true
 
 # GrapheneOS Camera
 TARGET_BUILD_GRAPHENEOS_CAMERA = true
